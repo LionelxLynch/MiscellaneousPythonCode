52a5c2f3db8e7ce0e73b613f1b29f362b23b8cd7
Lionel Lynch <Lionel@https://github.com/LionelxLynch/temp-node-project>
2022-01-28 00:21:19 -0500

    add and removed some programs

---
+++ lakearea.py
@@ -0,0 +1,18 @@
+#Lionel Lynch
+#lakearea.py
+#user inputs radius of a given body, the program returns area
+
+#decalre the pie, radius, and area asv constants
+pie = 3.1416
+radius = int(input('Enter the radius of the lake in meters: '))
+area = pie * (radius ** 2)
+
+#print result
+print(f'The are of the lake is {area} m^2')
+
+
+
+
+
+
+
