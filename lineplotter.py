b42b882a57ba0e491d0df02ca5491e48d39c337f
Lionel Lynch <Lionel@https://github.com/LionelxLynch/temp-node-project>
2022-02-21 21:23:50 -0500

    lineplotter.py comments added

--- lineplotter.py
+++ lineplotter.py
@@ -3,18 +3,23 @@
 #accept slope and intercept as input,
 #then output the X values
 
+
+#get slope and intercept from user via input
 slope = float(input('Enter the slope: '))
 intercept = float(input('Enter the intercept: '))
-print(f'Line {slope} * x + {intercept}')
-x = 0
 
+#show the line equation
+print(f'Line {slope} * x + {intercept}')
+
+#loop for 0 through 9
+x = 0
 while x <= 9:
     y = (slope * x) + intercept
     print(f'AT x = {x} , y = {y}')
     x = x + 1
 
+#loop for 10^2 up to 100,000
 x = 10
-
 while x <= 100000:
     y = (slope * x) + intercept
     print(f'AT x = {x} , y = {y}')
